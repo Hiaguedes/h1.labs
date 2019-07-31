27447186adc0e6a68c639de9a9f044b5ea7f8aa0
Hiaguedes <hiago_guedes94@hotmail.com>
2019-07-31 15:51:40 -0300

    commit

---
+++ Introducao Java Script/js/main.js
@@ -0,0 +1,33 @@
+﻿
+/*var lista = ["Pelé", "Xuxa", "Senna"];
+console.log(lista[1]);
+console.log(lista.join(" | "));
+console.log(lista);*/
+function setReplace(frase, nome, troca) {
+    return frase.setReplace(troca,nome);
+}
+
+function clicou() {
+    document.getElementById("Nome").innerText="Nossa Página";
+    
+}
+function muahaha() {
+
+    window.open("https://www.youtube.com/watch?v=tjAsZ6bHCZE");
+
+}
+
+function trocar_texto() {
+    document.getElementById("Botao2").innerHTML ="MUAHAHA  >:3"
+
+}
+
+function voltar_texto() {
+    document.getElementById("Botao2").innerHTML = "Nao Clique aqui :("
+
+}
+    function load() {
+        var nome = "Hiago Riba Guedes";
+        alert("Bem vindo Mestre  " + nome);
+
+    }
