65780f346a366e87721114dd85129468fca83b94
Hiaguedes <hiago_guedes94@hotmail.com>
2020-04-17 20:15:42 -0300

    angular no electron, rotas
    Primeiros passos no angular com electron, aplicando rotas e usando angular material

---
+++ integrate-angular/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+ let win;
+
+ function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600 });
+    if (process.env.DEBUG) {
+    win.loadURL(`http://localhost:4200`);
+    } else {
+    win.loadURL(`file://${__dirname}/dist/integrate-angular
+    /index.html`);
+    }
+    win.on('closed', () => {
+    win = null;
+    });
+    }
+
+
+ app.on('ready', createWindow);
+ app.on('window-all-closed', () => {
+ if (process.platform !== 'win32') {
+ app.quit();
+ }
+ });
+ app.on('activate', () => {
+ if (win === null) {
+    createWindow();
+}
+});
