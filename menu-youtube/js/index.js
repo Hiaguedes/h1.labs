20855aa8a83a68b88bbed2ff627a22961ac272fc
Hiaguedes <hiago_guedes94@hotmail.com>
2020-07-15 01:25:40 -0300

    adicionando o menu

--- menu-youtube/js/index.js
+++ menu-youtube/js/index.js
@@ -1,4 +1,4 @@
-document.querySelector(".toggle").addEventListener("change",function(event){
+document.querySelector(".toggle").addEventListener("change",function(){//dark-mode
     document.querySelector(".container").classList.toggle("inverse-header");
     document.querySelector("body").classList.toggle("inverse-body");
     document.querySelector(".input-search").classList.toggle("inv-input-search");
@@ -6,3 +6,15 @@ document.querySelector(".toggle").addEventListener("change",function(event){
     document.querySelector(".click-search").classList.toggle("inv-click-search");
     document.querySelector(".icon-search").classList.toggle("inv-icon-search");
 });
+
+document.querySelector(".menu").addEventListener("click",function(){
+    document.querySelector(".nav-container").classList.add("menu-nav");
+    document.querySelector(".nav-container").classList.remove("esconde");
+});
+
+document.querySelector(".menu-fechar").addEventListener("click",function(){
+    document.querySelector(".nav-container").classList.remove("menu-nav");
+    document.querySelector(".nav-container").classList.add("esconde");
+});
+
+
