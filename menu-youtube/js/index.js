5bc4333895055f04d2a73d7a1deacda373f65ac1
Hiaguedes <hiago_guedes94@hotmail.com>
2020-07-23 06:15:34 -0300

    finalizando menu-youtube

--- menu-youtube/js/index.js
+++ menu-youtube/js/index.js
@@ -2,17 +2,19 @@ document.querySelector(".toggle").addEventListener("change",function(){//dark-mo
     document.querySelector(".container").classList.toggle("inverse-header");
     document.querySelector("body").classList.toggle("inverse-body");
     document.querySelector(".input-search").classList.toggle("inv-input-search");
-    document.querySelector(".menu").classList.toggle("inv-menu");
     document.querySelector(".click-search").classList.toggle("inv-click-search");
     document.querySelector(".icon-search").classList.toggle("inv-icon-search");
+    document.querySelector(".nav-container").classList.toggle("inv-menu-nav")
+    document.querySelector(".menu").classList.toggle("inv-menu");
+    document.querySelector(".menu-fechar").classList.toggle("inv-menu");
 });
 
-document.querySelector(".menu").addEventListener("click",function(){
+document.querySelector(".menu").addEventListener("click",function(){//menu abre
     document.querySelector(".nav-container").classList.add("menu-nav");
     document.querySelector(".nav-container").classList.remove("esconde");
 });
 
-document.querySelector(".menu-fechar").addEventListener("click",function(){
+document.querySelector(".menu-fechar").addEventListener("click",function(){//menu fecha
     document.querySelector(".nav-container").classList.remove("menu-nav");
     document.querySelector(".nav-container").classList.add("esconde");
 });
