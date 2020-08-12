12098a4792a98fec816ec78bd341b4ca1bf7d48d
Hiaguedes <hiago_guedes94@hotmail.com>
2020-08-12 20:59:52 -0300

    arrumando titulo e dando arquitetura pro css

--- anima-svg/ReadMe.md
+++ anima-svg/ReadMe.md
@@ -1,3 +1,9 @@
 # anima-svg
 
 Apenas animar coisas de uma imagem que eu fiz no inkscape
+
+Para usar o sass
+
+```sass
+sass --watch assets/estilos.scss:assets/estilos.css  
+```
--- anima-svg/assets/js/putSvg.js
+++ anima-svg/assets/js/putSvg.js
@@ -1,6 +1,6 @@
 (()=>{
 
-    const divAnimacao=document.querySelector('.animacao');
+    const divAnimacao=document.querySelector('.header__animacao');
     let larguraJanela=window.innerWidth;
     resizeImg(larguraJanela);
 
@@ -31,14 +31,14 @@ function resizeImg(larguraJanela){
             arrangeByRowsAndColumns(4,3);
             break;
         case (larguraJanela<=320):
-            arrangeByRowsAndColumns(2,1)
+            arrangeByRowsAndColumns(2,2);
             break;
     }
 }
 
 function createImage(){
     const div=document.createElement('div');
-    div.classList.add('animacao__imagem');
+    div.classList.add('header__animacao--imagem');
     divAnimacao.appendChild(div);
 }
 
