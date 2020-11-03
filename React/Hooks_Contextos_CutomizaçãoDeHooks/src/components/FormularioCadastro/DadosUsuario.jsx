f4c77d1b9ef4a2ad984d03cd331fd83c6e8779a4
Hiaguedes <hiago_guedes94@hotmail.com>
2020-11-02 22:08:54 -0300

    fazendo renderização de partes do formulário com react

---
+++ React/Hooks_Contextos_CutomizaçãoDeHooks/src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Button, TextField} from '@material-ui/core';
+
+export default function DadosUsuario({aoEnviar}){
+    return (
+        <form onSubmit={(e)=> {
+            e.preventDefault();
+            aoEnviar();
+            }}>
+
+            <TextField 
+            id="email" 
+            label="email" 
+            type="email" 
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+            />
+
+            <TextField 
+            id="senha" 
+            label="senha" 
+            type="password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+            />
+
+            <Button 
+            type="submit"
+            variant="contained" 
+            color="primary"
+            >Cadastrar</Button>
+        </form>
+    )
+}
