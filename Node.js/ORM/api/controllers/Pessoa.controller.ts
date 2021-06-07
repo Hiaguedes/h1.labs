39619c467267c6d46c999b2d24660a8a12437115
Hiaguedes <hiago@contasimples.com>
2021-06-07 02:39:19 -0300

    finalizando crud na tabela de pessoas com ORM sequelize

--- Node.js/ORM/api/controllers/Pessoa.controller.ts
+++ Node.js/ORM/api/controllers/Pessoa.controller.ts
@@ -12,6 +12,54 @@ class PessoaController {
             return res.status(500).json(e.message)
         }
     }
+
+    static async findPersonById(req: Request, res: Response){
+        try {
+            const { id } = req.params;
+            const findPerson = await database.Pessoas.findOne({where: { id: Number(id) }});
+            return res.status(200).json(findPerson)
+        }
+
+        catch (e) {
+            return res.status(500).json(e.message)
+        }
+    }
+
+    static async createPerson(req: Request, res: Response){
+        const newPerson = req.body;
+
+        try {
+            const newPersonCreated = await database.Pessoas.create(newPerson);
+            return res.status(200).json(newPersonCreated);
+        } catch(e) {
+            return res.status(500).json(e.message)
+        }
+    }
+
+    static async deletePersonById(req: Request, res: Response){
+        const { id } = req.params;
+
+        try {
+            await database.Pessoas.destroy({where: { id: Number(id) }});
+            return res.status(200).json({message: 'Registro deletado'});
+        } catch(e) {
+            return res.status(500).json(e.message)
+        }
+    }
+
+    static async editPersonById(req: Request, res: Response){
+        const { id } = req.params;
+        const newPersonRegister = req.body;
+
+        try {
+            await database.Pessoas.update(newPersonRegister, {where: { id: Number(id) }} );
+
+            const findPerson = await database.Pessoas.findOne({where: { id: Number(id) }});
+            return res.status(200).json(findPerson);
+        } catch(e) {
+            return res.status(500).json(e.message)
+        }
+    }
 }
 
 module.exports = PessoaController;
--- Node.js/ORM/api/routes/pessoas.route.ts
+++ Node.js/ORM/api/routes/pessoas.route.ts
@@ -1,10 +1,13 @@
 const { Router } = require('express');
-import { Request, Response } from 'express';
 
 const PessoaController = require('../controllers/Pessoa.controller');
 
 const router = Router();
 
 router.get('/pessoas', PessoaController.findAll)
+router.get('/pessoas/:id', PessoaController.findPersonById)
+router.post('/pessoas', PessoaController.createPerson);
+router.delete('/pessoas/:id', PessoaController.deletePersonById);
+router.patch('/pessoas/:id', PessoaController.editPersonById);
 
 module.exports = router
