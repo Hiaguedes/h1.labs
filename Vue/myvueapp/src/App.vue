15989a19c47b91326450857d000d56ebab1839ba
Hiaguedes <hiago@contasimples.com>
2021-07-09 19:37:05 -0300

    comecando curso de vue

---
+++ Vue/myvueapp/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="display: flex;  flex-direction: column; align-items: center;">
+    <h1>{{ msg }}</h1>
+    <span>{{ author }}</span>
+    <button v-on:click="ChangeMsg">Clique</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    let dataSource = {
+        msg: 'Hello World!',
+        author: 'Hiago Riba Guedes'
+    }
+
+    return {...dataSource};
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ Vue/myvueapp/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  methods: {
+    ChangeMsg() {
+    console.log('chamado!')
+    this.msg += '!';
+  }
+}
+})
