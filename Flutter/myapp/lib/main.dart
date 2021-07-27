4b101f655eac71560ba6657ec28294dfe3554519
Hiaguedes <hiago@contasimples.com>
2021-07-27 01:05:52 -0300

    reaproveitando codigo flutter

--- Flutter/myapp/lib/main.dart
+++ Flutter/myapp/lib/main.dart
@@ -4,17 +4,50 @@ void main() => runApp(App());
 
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          'Olá',
-          textDirection: TextDirection.ltr,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferências'),
+          backgroundColor: Colors.green[400],
         ),
-        Text(
-          'Mundo',
-          textDirection: TextDirection.ltr,
-        )
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => {},
+          backgroundColor: Colors.green[400],
+        ),
+        body: ListaTransferencia(),
+      ),
+    );
+  }
+}
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemListaTransferencia('Hiago', '150.00'),
       ],
+    );
+  }
+}
+
+class ItemListaTransferencia extends StatelessWidget {
+  final String nomeConta;
+  final String valor;
+
+  ItemListaTransferencia(this.nomeConta, this.valor);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 1,
+      borderOnForeground: true,
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text('R\$' + valor),
+        subtitle: Text(nomeConta),
+      ),
     );
   }
 }
